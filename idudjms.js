28ccda8ab1d126e4d8133b7fbaeb96df9cd9689b
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2024-12-01 14:00:19 +0900

    Add files via upload

---
+++ idudjms.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    var vds = document.getElementById('vds');
+    
+    if (vds) {
+        vds.src = "https://cdn.jsdelivr.net/gh/rltzhdls/dkdjsks@main/IN3I4DXEGM256.mp4";
+        document.getElementById('vd').load();
+    }
+});
+
